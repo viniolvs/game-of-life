0f3af641398e894e709000019f4d5e7769237ad2
viniolvs <vinioliveira.s@hotmail.com>
2023-08-08 14:46:13 -0300

    new inputs

--- entry/gen_entrys.sh
+++ entry/gen_entrys.sh
@@ -5,3 +5,9 @@ for gen in 100 1000 5000; do
         ./newentry $size $gen > input_${size}_${gen}.in
     done
 done
+
+for gen in 100 1000 5000; do
+    for size in 10 100; do
+        ./newentry $size $gen > input_${size}_${gen}.in
+    done
+done
